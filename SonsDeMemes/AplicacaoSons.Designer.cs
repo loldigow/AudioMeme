1212d920d5e6ee5978b9ede00a1ed3e1e6f0cb9e
Rodrigo Ferreira de Almeida Wallauer <rodrigo.almeida@aliare.co>
2025-01-30 18:51:02 -0300

    Ajuste

--- SonsDeMemes/AplicacaoSons.Designer.cs
+++ SonsDeMemes/AplicacaoSons.Designer.cs
@@ -32,119 +32,164 @@ namespace SonsDeMemes
         {
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AplicacaoSons));
-            button1 = new MeuBotao();
-            button2 = new MeuBotao();
-            button3 = new MeuBotao();
-            button4 = new MeuBotao();
-            button5 = new MeuBotao();
-            button6 = new MeuBotao();
-            button7 = new MeuBotao();
-            button8 = new MeuBotao();
-            button9 = new MeuBotao();
+            btnMusica1 = new MeuBotao();
+            btnMusica2 = new MeuBotao();
+            btnMusica3 = new MeuBotao();
+            btnMusica4 = new MeuBotao();
+            btnMusica5 = new MeuBotao();
+            btnMusica6 = new MeuBotao();
+            btnMusica7 = new MeuBotao();
+            btnMusica8 = new MeuBotao();
+            btnMusica9 = new MeuBotao();
             iconMinimizado = new NotifyIcon(components);
+            btnMusica10 = new MeuBotao();
+            btnMusica11 = new MeuBotao();
+            btnMusica12 = new MeuBotao();
             SuspendLayout();
             // 
-            // button1
+            // btnMusica1
             // 
-            button1.Location = new Point(12, 38);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 86);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            btnMusica1.Location = new Point(12, 38);
+            btnMusica1.Model = null;
+            btnMusica1.Name = "btnMusica1";
+            btnMusica1.Size = new Size(94, 86);
+            btnMusica1.TabIndex = 0;
+            btnMusica1.Text = "<vazio>";
+            btnMusica1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnMusica2
             // 
-            button2.Location = new Point(112, 38);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 86);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            btnMusica2.Location = new Point(112, 38);
+            btnMusica2.Model = null;
+            btnMusica2.Name = "btnMusica2";
+            btnMusica2.Size = new Size(94, 86);
+            btnMusica2.TabIndex = 1;
+            btnMusica2.Text = "<vazio>";
+            btnMusica2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnMusica3
             // 
-            button3.Location = new Point(212, 38);
-            button3.Name = "button3";
-            button3.Size = new Size(94, 86);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            btnMusica3.Location = new Point(212, 38);
+            btnMusica3.Model = null;
+            btnMusica3.Name = "btnMusica3";
+            btnMusica3.Size = new Size(94, 86);
+            btnMusica3.TabIndex = 2;
+            btnMusica3.Text = "<vazio>";
+            btnMusica3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnMusica4
             // 
-            button4.Location = new Point(12, 130);
-            button4.Name = "button4";
-            button4.Size = new Size(94, 86);
-            button4.TabIndex = 5;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
+            btnMusica4.Location = new Point(312, 38);
+            btnMusica4.Model = null;
+            btnMusica4.Name = "btnMusica4";
+            btnMusica4.Size = new Size(94, 86);
+            btnMusica4.TabIndex = 3;
+            btnMusica4.Text = "<vazio>";
+            btnMusica4.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnMusica5
             // 
-            button5.Location = new Point(112, 130);
-            button5.Name = "button5";
-            button5.Size = new Size(94, 86);
-            button5.TabIndex = 4;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
+            btnMusica5.Location = new Point(12, 130);
+            btnMusica5.Model = null;
+            btnMusica5.Name = "btnMusica5";
+            btnMusica5.Size = new Size(94, 86);
+            btnMusica5.TabIndex = 4;
+            btnMusica5.Text = "<vazio>";
+            btnMusica5.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnMusica6
             // 
-            button6.Location = new Point(211, 130);
-            button6.Name = "button6";
-            button6.Size = new Size(94, 86);
-            button6.TabIndex = 3;
-            button6.Text = "button6";
-            button6.UseVisualStyleBackColor = true;
+            btnMusica6.Location = new Point(112, 130);
+            btnMusica6.Model = null;
+            btnMusica6.Name = "btnMusica6";
+            btnMusica6.Size = new Size(94, 86);
+            btnMusica6.TabIndex = 5;
+            btnMusica6.Text = "<vazio>";
+            btnMusica6.UseVisualStyleBackColor = true;
             // 
-            // button7
+            // btnMusica7
             // 
-            button7.Location = new Point(12, 222);
-            button7.Name = "button7";
-            button7.Size = new Size(94, 86);
-            button7.TabIndex = 8;
-            button7.Text = "button7";
-            button7.UseVisualStyleBackColor = true;
+            btnMusica7.Location = new Point(211, 130);
+            btnMusica7.Model = null;
+            btnMusica7.Name = "btnMusica7";
+            btnMusica7.Size = new Size(94, 86);
+            btnMusica7.TabIndex = 6;
+            btnMusica7.Text = "<vazio>";
+            btnMusica7.UseVisualStyleBackColor = true;
             // 
-            // button8
+            // btnMusica8
             // 
-            button8.Location = new Point(112, 222);
-            button8.Name = "button8";
-            button8.Size = new Size(94, 86);
-            button8.TabIndex = 7;
-            button8.Text = "button8";
-            button8.UseVisualStyleBackColor = true;
+            btnMusica8.Location = new Point(312, 130);
+            btnMusica8.Model = null;
+            btnMusica8.Name = "btnMusica8";
+            btnMusica8.Size = new Size(94, 86);
+            btnMusica8.TabIndex = 7;
+            btnMusica8.Text = "<vazio>";
+            btnMusica8.UseVisualStyleBackColor = true;
             // 
-            // button9
+            // btnMusica9
             // 
-            button9.Location = new Point(211, 222);
-            button9.Name = "button9";
-            button9.Size = new Size(94, 86);
-            button9.TabIndex = 6;
-            button9.Text = "button9";
-            button9.UseVisualStyleBackColor = true;
+            btnMusica9.Location = new Point(12, 222);
+            btnMusica9.Model = null;
+            btnMusica9.Name = "btnMusica9";
+            btnMusica9.Size = new Size(94, 86);
+            btnMusica9.TabIndex = 8;
+            btnMusica9.Text = "<vazio>";
+            btnMusica9.UseVisualStyleBackColor = true;
             // 
             // iconMinimizado
             // 
-            iconMinimizado.Icon = (Icon)resources.GetObject("$this.Icon");
+            iconMinimizado.Icon = (Icon)resources.GetObject("iconMinimizado.Icon");
             iconMinimizado.Text = "Meme sons";
             iconMinimizado.Visible = true;
+            // 
+            // btnMusica10
+            // 
+            btnMusica10.Location = new Point(112, 222);
+            btnMusica10.Model = null;
+            btnMusica10.Name = "btnMusica10";
+            btnMusica10.Size = new Size(94, 86);
+            btnMusica10.TabIndex = 9;
+            btnMusica10.Text = "<vazio>";
+            btnMusica10.UseVisualStyleBackColor = true;
+            // 
+            // btnMusica11
+            // 
+            btnMusica11.Location = new Point(212, 222);
+            btnMusica11.Model = null;
+            btnMusica11.Name = "btnMusica11";
+            btnMusica11.Size = new Size(94, 86);
+            btnMusica11.TabIndex = 10;
+            btnMusica11.Text = "<vazio>";
+            btnMusica11.UseVisualStyleBackColor = true;
+            // 
+            // btnMusica12
+            // 
+            btnMusica12.Location = new Point(312, 222);
+            btnMusica12.Model = null;
+            btnMusica12.Name = "btnMusica12";
+            btnMusica12.Size = new Size(94, 86);
+            btnMusica12.TabIndex = 11;
+            btnMusica12.Text = "<vazio>";
+            btnMusica12.UseVisualStyleBackColor = true;
             // 
             // AplicacaoSons
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(317, 338);
-            Controls.Add(button7);
-            Controls.Add(button8);
-            Controls.Add(button9);
-            Controls.Add(button4);
-            Controls.Add(button5);
-            Controls.Add(button6);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(418, 338);
+            Controls.Add(btnMusica12);
+            Controls.Add(btnMusica11);
+            Controls.Add(btnMusica10);
+            Controls.Add(btnMusica7);
+            Controls.Add(btnMusica8);
+            Controls.Add(btnMusica9);
+            Controls.Add(btnMusica4);
+            Controls.Add(btnMusica5);
+            Controls.Add(btnMusica6);
+            Controls.Add(btnMusica3);
+            Controls.Add(btnMusica2);
+            Controls.Add(btnMusica1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "AplicacaoSons";
             Text = "Meme icons";
@@ -153,15 +198,18 @@ namespace SonsDeMemes
 
         #endregion
 
-        private MeuBotao button1;
-        private MeuBotao button2;
-        private MeuBotao button3;
-        private MeuBotao button4;
-        private MeuBotao button5;
-        private MeuBotao button6;
-        private MeuBotao button7;
-        private MeuBotao button8;
-        private MeuBotao button9;
+        private MeuBotao btnMusica1;
+        private MeuBotao btnMusica2;
+        private MeuBotao btnMusica3;
+        private MeuBotao btnMusica4;
+        private MeuBotao btnMusica5;
+        private MeuBotao btnMusica6;
+        private MeuBotao btnMusica7;
+        private MeuBotao btnMusica8;
+        private MeuBotao btnMusica9;
         private NotifyIcon iconMinimizado;
+        private MeuBotao btnMusica10;
+        private MeuBotao btnMusica11;
+        private MeuBotao btnMusica12;
     }
 }
--- SonsDeMemes/AplicacaoSons.cs
+++ SonsDeMemes/AplicacaoSons.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.Devices;
 using NAudio.Wave;
+using Newtonsoft.Json;
 using SonsDeMemes.Custons;
 using System.ComponentModel;
+using System.Reflection;
+using System.Xml;
 
 
 namespace SonsDeMemes
@@ -11,16 +15,98 @@ namespace SonsDeMemes
         private bool _closeRequested;
         private WaveOutEvent outputDevice;
         private Mp3FileReader mp3Reader;
+        private List<BotaoModel> _modelos = new List<BotaoModel>();
+        private static string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "audios.json");
+
+
         public AplicacaoSons()
         {
             InitializeComponent();
             InicializeIconeMinimizado();
-            for (int i = 1; i <= 9; i++)
+            CarregueAudios(); 
+            foreach (var control in this.Controls)
             {
-                var viewModel = new BotaoModel { Nome = $"Botao {i}" };
-                var botao = this.Controls.Find($"button{i}", true).FirstOrDefault() as MeuBotao;
-                botao.Click += Botao_Click;
-                botao.Model = viewModel;
+                if (control is MeuBotao botao && botao.Name.Contains("btnMusica"))
+                {
+                    var sequencia = new string(botao.Name.Where(char.IsDigit).ToArray());
+                    if(int.TryParse(sequencia, out int i))
+                    {
+                        var musica = _modelos.FirstOrDefault(x => x.Sequencia == i) ?? new BotaoModel { Nome = $"<vazio>", Sequencia = i };
+                        botao.MouseUp += Botao_MouseUp;
+                        botao.Model = musica;
+                        botao.BackColor = musica.Backgroud;
+                        botao.Text = musica.Nome;
+                    }
+
+                    //musica = _modelos.FirstOrDefault(x => x.Sequencia == ++i) ?? new BotaoModel { Nome = $"Botao {botao.Name}", Sequencia = i }; ;
+                    //botao.MouseUp += Botao_MouseUp; // Adiciona o evento MouseUp
+                    //botao.Model = musica;
+                }
+            }
+        }
+
+        private void CarregueAudios()
+        {
+            if (!File.Exists(jsonFilePath)) 
+                return;
+
+            string json = File.ReadAllText(jsonFilePath);
+            _modelos = JsonConvert.DeserializeObject<List<BotaoModel>>(json);
+        }
+
+        private void Botao_MouseUp(object? sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Botao_Click(sender, e);
+            }
+            else
+            {
+                AdicioneOuSubstituaSom(sender, e);
+            }
+        }
+
+        private void AdicioneOuSubstituaSom(object? sender, EventArgs e)
+        {
+            if (sender is MeuBotao botao && botao.Model is BotaoModel model)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "MP3 Files (*.mp3)|*.mp3|All Files (*.*)|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    string fileName = Path.GetFileNameWithoutExtension(filePath);
+                    byte[] audioBytes = File.ReadAllBytes(filePath);
+                    string nomeMusica = Interaction.InputBox("Digite o nome da música:", "Nome da Música", fileName);
+                    model.Nome = nomeMusica;
+                    model.Som = audioBytes;
+                    botao.BackColor = Color.LightGray;
+                    model.Backgroud = Color.LightGray;
+                    botao.Text = nomeMusica;
+
+                    SalveMusicaLocal(model);
+
+                }
+            }
+        }
+
+        private void SalveMusicaLocal(BotaoModel model)
+        {
+            try
+            {
+                var musica = _modelos.FirstOrDefault(x => x.Sequencia == model.Sequencia);
+                if (musica != null)
+                {
+                    _modelos.Remove(musica);
+                }
+                _modelos.Add(model);
+                string json = JsonConvert.SerializeObject(_modelos);
+                File.WriteAllText(jsonFilePath, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao tentar salvar a música: {ex.Message}");
             }
         }
 
@@ -28,62 +114,46 @@ namespace SonsDeMemes
         {
             if (sender is MeuBotao botao && botao.Model is BotaoModel model)
             {
-                if (model.Som != null)
+                if (model.Som == null)
                 {
-                    try
+                    AdicioneOuSubstituaSom(sender, e);
+                    return;
+                }
+
+
+                try
+                {
+                    if (model.Running)
                     {
-                        if (model.Running)
-                        {
-                            outputDevice?.Stop();
-                            outputDevice?.Dispose();
-                            mp3Reader?.Dispose();
-                            model.Running = false;
-                        }
-                        else
-                        {
-                            outputDevice?.Stop();
-                            outputDevice?.Dispose();
-                            mp3Reader?.Dispose();
-                            MemoryStream ms = new MemoryStream(model.Som);
-                            mp3Reader = new Mp3FileReader(ms);
-                            outputDevice = new WaveOutEvent();
-                            outputDevice.PlaybackStopped += (sender, args) =>
-                            {
-                                model.Running = false;
-                            };
-                            outputDevice.Init(mp3Reader);
-                            outputDevice.Play();
-                            model.Running = true;
-                        }
+                        outputDevice?.Stop();
+                        outputDevice?.Dispose();
+                        mp3Reader?.Dispose();
+                        model.Running = false;
                     }
-                    catch (Exception ex)
+                    else
                     {
-                        MessageBox.Show($"Erro ao tentar reproduzir o áudio: {ex.Message}");
+                        outputDevice?.Stop();
+                        outputDevice?.Dispose();
+                        mp3Reader?.Dispose();
+                        MemoryStream ms = new MemoryStream(model.Som);
+                        mp3Reader = new Mp3FileReader(ms);
+                        outputDevice = new WaveOutEvent();
+                        outputDevice.PlaybackStopped += (sender, args) =>
+                        {
+                            model.Running = false;
+                            botao.BackColor = Color.LightGray;
+                        };
+                        outputDevice.Init(mp3Reader);
+                        outputDevice.Play();
+                        model.Running = true;
+                        botao.BackColor = Color.Gray;
                     }
                 }
-                else
+                catch (Exception ex)
                 {
-                    OpenFileDialog openFileDialog = new OpenFileDialog();
-                    openFileDialog.Filter = "MP3 Files (*.mp3)|*.mp3|All Files (*.*)|*.*";
-
-                    if (openFileDialog.ShowDialog() == DialogResult.OK)
-                    {
-                        string filePath = openFileDialog.FileName;
-                        string fileName = Path.GetFileNameWithoutExtension(filePath);
-                        byte[] audioBytes = File.ReadAllBytes(filePath);
-                        string nomeMusica = Interaction.InputBox("Digite o nome da música:", "Nome da Música", fileName);
-                        model.Nome = nomeMusica;
-                        model.Som = audioBytes;
-                        botao.BackColor = Color.Green;
-                        botao.Text = nomeMusica;
-                    }
+                    MessageBox.Show($"Erro ao tentar reproduzir o áudio: {ex.Message}");
                 }
             }
-        }
-
-        private void OutputDevice_PlaybackStopped(object? sender, StoppedEventArgs e)
-        {
-            throw new NotImplementedException();
         }
 
         private void InicializeIconeMinimizado()
@@ -103,7 +173,7 @@ namespace SonsDeMemes
             iconMinimizado.ContextMenuStrip.Items.Add("Finalizar", null, (e, f) =>
             {
                 _closeRequested = true;
-                System.Windows.Forms.Application.Exit();
+                Application.Exit();
             });
             Hide();
         }
@@ -127,12 +197,15 @@ namespace SonsDeMemes
             e.Cancel = true;
             InicializeIconeMinimizado();
         }
+
     }
 
     public class BotaoModel
     {
+        public int Sequencia { get; set; }
         public string Nome { get; set; }
         public byte[] Som { get; set; }
         public bool Running { get; set; }
+        public Color Backgroud { get; set; }
     }
 }
